852881a4625539c9d877bf0b222fd513724c0a33
eho <edvard.holst@gmail.com>
2019-01-15 00:18:42 +0100

    Only stop charging if current charging state is "CHARGING"

--- examples/max_soc.py
+++ examples/max_soc.py
@@ -3,25 +3,24 @@ import threading
 
 
 max_soc = 80  # SET MAX SOC LEVEL HERE
-max_soc_reached = False
 
 
 def check_soc():
-    global max_soc_reached
     threading.Timer(60.0, check_soc).start() # Called every minute
+
+    status = v.get_status()['vehicleStatus']
     # Get current soc
-    current_soc = int(v.get_status()['vehicleStatus'][38].get('value'))
-    if current_soc >= max_soc and not max_soc_reached:
-        # Stop charging
+    current_soc = int(status[38].get('value'))
+    # Get current charging state
+    charging_state = status[6]
+    if current_soc >= max_soc and charging_state is "CHARGING":
+        # Stop charging if we are currently charging
         v.charging_stop()
-        max_soc_reached = True
-    else:
-        max_soc_reached = False
 
 
 c = jlrpy.Connection('user@email.com', 'password')
 v = c.vehicles[0]
 
-print("[*] Connection established. Enforcing max soc")
+print("[*] Connection established. Enforcing max soc of %d%%" % max_soc)
 check_soc()
 
