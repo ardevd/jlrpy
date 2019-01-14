b8eefdac88a962a3357ffa20df08ce47017f5409
eho <edvard.holst@gmail.com>
2019-01-15 00:28:33 +0100

    Added ability to set minimum state of charge

--- examples/max_soc.py
+++ examples/max_soc.py
@@ -4,6 +4,9 @@
 This script will check the charging level once every minute
 and if the current battery level meets or exceeds the specified
 maximum value, the charging will stop if the vehicle is currently charging.
+
+You can also specify a minimum charging level and ensure charging is started
+if the battery level drops below the specified minimum state of charge.
 """
 
 import jlrpy
@@ -11,11 +14,12 @@ import threading
 
 
 max_soc = 80  # SET MAX SOC LEVEL HERE (percentage)
+min_soc = 20  # SET MIN SOC LEVEL HERE
 
 
 def check_soc():
-    """Retrieve vehicle status and stop charging if
-    current charging level matches or exceeds specified max level and
+    """Retrieve vehicle status and stop or start charging if
+    current charging level matches or exceeds specified max/min level and
     the vehicle is currently charging.
     """
     threading.Timer(60.0, check_soc).start()  # Called every minute
@@ -28,11 +32,15 @@ def check_soc():
     if current_soc >= max_soc and charging_state is "CHARGING":
         # Stop charging if we are currently charging
         v.charging_stop()
+    elif current_soc < min_soc and charging_state is "NOT CHARGING":
+        # Start charging
+        v.charging_start()
 
 
 c = jlrpy.Connection('user@email.com', 'password')
 v = c.vehicles[0]
 
-print("[*] Connection established. Enforcing max soc of %d%%" % max_soc)
+print("[*] Enforcing max soc of %d%%" % max_soc)
+print("[*] Enforcing min soc of %d%%" % min_soc)
 check_soc()
 
