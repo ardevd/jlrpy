ff1aa039ef37bb3453bb6cd57c06c67778c3e369
eho <edvard.holst@gmail.com>
2019-01-14 23:58:10 +0100

    Do not attempt to stop charging if we have already stopped it

--- examples/max_soc.py
+++ examples/max_soc.py
@@ -2,19 +2,25 @@ import jlrpy
 import threading
 
 
+max_soc = 80  # SET MAX SOC LEVEL HERE
+max_soc_reached = False
+
+
 def check_soc():
+    global max_soc_reached
     threading.Timer(60.0, check_soc).start() # Called every minute
     # Get current soc
     current_soc = int(v.get_status()['vehicleStatus'][38].get('value'))
-    if current_soc >= max_soc:
+    if current_soc >= max_soc and not max_soc_reached:
         # Stop charging
         v.charging_stop()
+        max_soc_reached = True
+    else:
+        max_soc_reached = False
 
 
 c = jlrpy.Connection('user@email.com', 'password')
 v = c.vehicles[0]
-
-max_soc = 80  # SET MAX SOC LEVEL HERE
 
 print("[*] Connection established. Enforcing max soc")
 check_soc()
