9a3af34b407a3dad3c95cacdc1e5aee82a6b72e8
eho <edvard.holst@gmail.com>
2019-01-14 23:47:26 +0100

    Formatting fixes

--- examples/max_soc.py
+++ examples/max_soc.py
@@ -1,6 +1,7 @@
 import jlrpy
 import threading
 
+
 def check_soc():
     threading.Timer(60.0, check_soc).start() # Called every minute
     # Get current soc
@@ -8,11 +9,12 @@ def check_soc():
     if current_soc >= max_soc:
         # Stop charging
         v.charging_stop()
-        
+
+
 c = jlrpy.Connection('user@email.com', 'password')
 v = c.vehicles[0]
 
-max_soc = 80 # SET MAX SOC LEVEL HERE
+max_soc = 80  # SET MAX SOC LEVEL HERE
 
 print("[*] Connection established. Enforcing max soc")
 check_soc()
