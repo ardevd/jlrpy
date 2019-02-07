fadc39fc751e69eb91fca0f70f42cbe67f703131
eho <edvard.holst@gmail.com>
2019-02-07 13:48:29 +0100

    Added required Content-Type and missing header argument

--- jlrpy.py
+++ jlrpy.py
@@ -148,7 +148,9 @@ class Connection(object):
 
     def update_user_info(self, user_info_data):
         """Update user information"""
-        return self.post(self.user_id, "https://jlp-ifoa.wirelesscar.net/if9/jlr/users", user_info_data)
+        headers = self.head.copy()
+        headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.User-v3+json; charset=utf-8"
+        return self.post(self.user_id, "https://jlp-ifoa.wirelesscar.net/if9/jlr/users", headers, user_info_data)
 
     def reverse_geocode(self, lat, lon):
         """Get geocode information"""
