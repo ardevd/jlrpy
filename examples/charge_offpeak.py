463c6608854f0ad8e8868e1d1ab6a37a1c35c526
Herbert van den Bergh <herbert.van.den.bergh@oracle.com>
2019-03-10 20:36:16 -0700

    Remove extra line breaks

--- examples/charge_offpeak.py
+++ examples/charge_offpeak.py
@@ -62,10 +62,7 @@ def distance(origin, destination):
          math.sin(dlon / 2) * math.sin(dlon / 2))
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     d = radius * c
-
     return d
-
-
 
 def check_soc():
     """Retrieve vehicle status and stop or start charging if
