675b25a1c34c2f266cbc0b1146569aae3c223dd9
eho <edvard.holst@gmail.com>
2019-10-07 22:05:27 +0200

    updated version info for v1.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jlrpy",
-    version="1.3.0",
+    version="1.3.1",
     author="Edvard",
     author_email="edvard.holst@gmail.com",
     description="Control your Jaguar I-Pace",
