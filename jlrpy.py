e2b9ab023432b69454d860934ddb12667f28b15e
ardevd <ardevd@users.noreply.github.com>
2020-06-22 13:30:42 +0200

    Add Guardian Mode support (#73)
    * guardian mode: added Guardian Mode GET requests.
    
    Added ability to get Guardian Mode alarms, alerts, settings and status
    
    * added the ability to enable guardian mode

--- jlrpy.py
+++ jlrpy.py
@@ -251,6 +251,38 @@ class Vehicle(dict):
         headers["Accept"] = "application/vnd.ngtp.org.triplist-v2+json"
         return self.get('trips?count=%d' % count, headers)
 
+    def get_guardian_mode_alarms(self):
+        """Get Guardian Mode Alarms"""
+        headers = self.connection.head.copy()
+        headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.GuardianStatus-v1+json"
+        headers["Accept-Encoding"] = "gzip,deflate"
+        return self.get('gm/alarms', headers)
+
+    def get_guardian_mode_alerts(self):
+        """Get Guardian Mode Alerts"""
+        headers = self.connection.head.copy()
+        headers["Accept"] = "application/wirelesscar.GuardianAlert-v1+json"
+        headers["Accept-Encoding"] = "gzip,deflate"
+        return self.get('gm/alerts', headers)
+
+    def get_guardian_mode_status(self):
+        """Get Guardian Mode Status"""
+        headers = self.connection.head.copy()
+        headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.GuardianStatus-v1+json"
+        return self.get('gm/status', headers)
+
+    def get_guardian_mode_settings_user(self):
+        """Get Guardian Mode User Settings"""
+        headers = self.connection.head.copy()
+        headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.GuardianUserSettings-v1+json"
+        return self.get('gm/settings/user', headers)
+
+    def get_guardian_mode_settings_system(self):
+        """Get Guardian Mode System Settings"""
+        headers = self.connection.head.copy()
+        headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.GuardianSystemSettings-v1+json"
+        return self.get('gm/settings/system', headers)
+
     def get_trip(self, trip_id):
         """Get info on a specific trip"""
         return self.get('trips/%s/route?pageSize=1000&page=0' % trip_id, self.connection.head)
@@ -488,6 +520,10 @@ class Vehicle(dict):
         """Enable service mode. Will disable at the specified time (epoch millis)"""
         return self._prov_command(pin, expiration_time, "protectionStrategy_serviceMode")
 
+    def enable_guardian_mode(self, pin, expiration_time):
+        """Enable Guardian Mode until the specified time (epoch millis)"""
+        return self._gm_command(pin, expiration_time, "ACTIVE")
+
     def enable_transport_mode(self, pin, expiration_time):
         """Enable transport mode. Will be disabled at the specified time (epoch millis)"""
         return self._prov_command(pin, expiration_time, "protectionStrategy_transportMode")
@@ -511,6 +547,16 @@ class Vehicle(dict):
         prov_data["endTime"] = expiration_time
 
         return self.post("prov", headers, prov_data)
+
+    def _gm_command(self, pin, expiration_time, status):
+        """Send GM toggle command"""
+        headers = self.connection.head.copy()
+        headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.GuardianAlarmList-v1+json"
+        gm_data = self.authenticate_gm(pin)
+        gm_data["endTime"] = expiration_time
+        gm_data["status"] = status
+
+        return self.post("gm/alarms", headers, gm_data)
 
     def _authenticate_vhs(self):
         """Authenticate to vhs and get token"""
@@ -574,6 +620,9 @@ class Vehicle(dict):
     def authenticate_prov(self, pin):
         """Authenticate to PROV service"""
         return self._authenticate_pin_protected_service(pin, "PROV")
+
+    def authenticate_gm(self, pin):
+        return self._authenticate_pin_protected_service(pin, "GM")
 
     def _authenticate_pin_protected_service(self, pin, service_name):
         """Authenticate to specified service with the provided PIN"""
