f6f2ecb416910c6fbfbecb6b44e72790f1542898
Mark Parker <msparker@sky.com>
2023-07-19 20:49:14 +0100

    refactor for modern standards

--- jlrpy.py
+++ jlrpy.py
@@ -2,20 +2,36 @@
 https://github.com/ardevd/jlrpy
 """
 
-from urllib.request import Request, build_opener
 
-import json
-import datetime
 import calendar
-import uuid
-import sys
+import enum
+import json
 import logging
+import time
+import uuid
+from datetime import datetime
+
+import requests
+from requests.exceptions import HTTPError
 
 logger = logging.getLogger('jlrpy')
 
-IFAS_BASE_URL = "https://ifas.prod-row.jlrmotor.com/ifas/jlr"
-IFOP_BASE_ULR = "https://ifop.prod-row.jlrmotor.com/ifop/jlr"
-IF9_BASE_URL = "https://if9.prod-row.jlrmotor.com/if9/jlr"
+
+class BaseURLs(enum.Enum):
+    """Rest Of World Base URLs"""
+    IFAS = "https://ifas.prod-row.jlrmotor.com/ifas/jlr"
+    IFOP = "https://ifop.prod-row.jlrmotor.com/ifop/jlr"
+    IF9 = "https://if9.prod-row.jlrmotor.com/if9/jlr"
+
+
+class ChinaBaseURLs(enum.Enum):
+    """China Base URLs"""
+    IFAS = "https://ifas.prod-chn.jlrmotor.com/ifas/jlr"
+    IFOP = "https://ifop.prod-chn.jlrmotor.com/ifop/jlr"
+    IF9 = "https://ifoa.prod-chn.jlrmotor.com/if9/jlr"
+
+
+TIMEOUT = 15
 
 
 class Connection:
@@ -33,15 +49,19 @@ class Connection:
         A device Id can optionally be specified. If not one will be generated at runtime.
         A refresh token can be supplied for authentication instead of a password
         """
-        self.email = email
+        self.email: str = email
+        self.expiration: int = 0  # force credential refresh
+        self.access_token: str
+        self.auth_token: str
+        self.head: dict = {}
+        self.refresh_token: str
+        self.user_id: str
+        self.vehicles: list = []
 
         if use_china_servers:
-            global IFAS_BASE_URL
-            IFAS_BASE_URL = "https://ifas.prod-chn.jlrmotor.com/ifas/jlr"
-            global IFOP_BASE_ULR
-            IFOP_BASE_ULR = "https://ifop.prod-chn.jlrmotor.com/ifop/jlr"
-            global IF9_BASE_URL
-            IF9_BASE_URL = "https://ifoa.prod-chn.jlrmotor.com/if9/jlr"
+            self.base = ChinaBaseURLs
+        else:
+            self.base = BaseURLs
 
         if device_id:
             self.device_id = device_id
@@ -58,88 +78,89 @@ class Connection:
                 "username": email,
                 "password": password}
 
-        self.expiration = 0  # force credential refresh
-
         self.connect()
 
-        self.vehicles = []
         try:
-            for v in self.get_vehicles(self.head)['vehicles']:
-                self.vehicles.append(Vehicle(v, self))
+            for vehicle in self.get_vehicles(self.head)['vehicles']:
+                self.vehicles.append(Vehicle(vehicle, self))
         except TypeError:
             logger.error("No vehicles associated with this account")
 
-    def get(self, command, url, headers):
-        """GET data from API"""
-        return self.post(command, url, headers, None)
-
-    def post(self, command, url, headers, data=None):
-        """POST data to API"""
-        now = calendar.timegm(datetime.datetime.now().timetuple())
-        logger.debug(url)
+    def validate_token(self):
+        """Is token still valid"""
+        now = calendar.timegm(datetime.now().timetuple())
         if now > self.expiration:
             # Auth expired, reconnect
             self.connect()
-            if headers['Authorization']:
-                headers['Authorization'] = self.head['Authorization']
-        return self.__open("%s/%s" % (url, command), headers=headers, data=data)
+
+    def get(self, command, url, headers):
+        """GET data from API"""
+        self.validate_token()
+        if headers['Authorization']:
+            headers['Authorization'] = self.head['Authorization']
+        return self._request(f"{url}/{command}", headers=headers, method="GET")
+
+    def post(self, command, url, headers, data=None):
+        """POST data to API"""
+        self.validate_token()
+        if headers['Authorization']:
+            headers['Authorization'] = self.head['Authorization']
+        return self._request(f"{url}/{command}", headers=headers, data=data, method="POST")
 
     def connect(self):
+        """Connect to JLR API"""
         logger.info("Connecting...")
-        auth = self.__authenticate(data=self.oauth)
-        self.__register_auth(auth)
-        self.__set_header(auth['access_token'])
+        auth = self._authenticate(data=self.oauth)
+        self._register_auth(auth)
+        self._set_header(auth['access_token'])
         logger.info("[+] authenticated")
-        self.__register_device_and_log_in()
+        self._register_device_and_log_in()
 
-    def __register_device_and_log_in(self):
-        self.__register_device(self.head)
+    def _register_device_and_log_in(self):
+        self._register_device(self.head)
         logger.info("1/2 device id registered")
-        self.__login_user(self.head)
+        self._login_user(self.head)
         logger.info("2/2 user logged in, user id retrieved")
 
-    def __open(self, url, headers=None, data=None):
-        req = Request(url, headers=headers)
-        if data:
-            req.data = bytes(json.dumps(data), encoding="utf8")
+    def _request(self, url, headers=None, data=None, method="GET"):
+        print(f"URL: {url} \nHEADERS: {headers} \nDATA: {data} \nMETHOD: {method}")
+        ret = requests.request(method=method, url=url, headers=headers, json=data, timeout=TIMEOUT)
+        print(ret.text)
+        if ret.text:
+            try:
+                return json.loads(ret.text)
+            except json.JSONDecodeError:
+                return None
+        return None
 
-        opener = build_opener()
-        resp = opener.open(req)
-        charset = resp.info().get('charset', 'utf-8')
-        resp_data = resp.read().decode(charset)
-        if resp_data:
-            return json.loads(resp_data)
-        else:
-            return None
-
-    def __register_auth(self, auth):
+    def _register_auth(self, auth):
         self.access_token = auth['access_token']
-        now = calendar.timegm(datetime.datetime.now().timetuple())
+        now = calendar.timegm(datetime.now().timetuple())
         self.expiration = now + int(auth['expires_in'])
         self.auth_token = auth['authorization_token']
         self.refresh_token = auth['refresh_token']
 
-    def __set_header(self, access_token):
+    def _set_header(self, access_token):
         """Set HTTP header fields"""
         self.head = {
-            "Authorization": "Bearer %s" % access_token,
+            "Authorization": f"Bearer {access_token}",
             "X-Device-Id": self.device_id,
             "x-telematicsprogramtype": "jlrpy",
             "Content-Type": "application/json"}
 
-    def __authenticate(self, data=None):
+    def _authenticate(self, data=None):
         """Raw urlopen command to the auth url"""
-        url = "%s/tokens" % IFAS_BASE_URL
+        url = f"{self.base.IFAS}/tokens"
         auth_headers = {
             "Authorization": "Basic YXM6YXNwYXNz",
             "Content-Type": "application/json",
             "X-Device-Id": self.device_id}
 
-        return self.__open(url, auth_headers, data)
+        return self._request(url, auth_headers, data, "POST")
 
-    def __register_device(self, headers=None):
+    def _register_device(self, headers=None):
         """Register the device Id"""
-        url = "%s/users/%s/clients" % (IFOP_BASE_ULR, self.email)
+        url = f"{self.base.IFOP}/users/{self.email}/clients"
         data = {
             "access_token": self.access_token,
             "authorization_token": self.auth_token,
@@ -147,15 +168,15 @@ class Connection:
             "deviceID": self.device_id
         }
 
-        return self.__open(url, headers, data)
+        return self._request(url, headers, data, "POST")
 
-    def __login_user(self, headers=None):
+    def _login_user(self, headers=None):
         """Login the user"""
-        url = "%s/users?loginName=%s" % (IF9_BASE_URL, self.email)
+        url = f"{self.base.IF9}/users?loginName={self.email}"
         user_login_header = headers.copy()
         user_login_header["Accept"] = "application/vnd.wirelesscar.ngtp.if9.User-v3+json"
 
-        user_data = self.__open(url, user_login_header)
+        user_data = self._request(url, user_login_header)
         self.user_id = user_data['userId']
         return user_data
 
@@ -165,32 +186,30 @@ class Connection:
             "grant_type": "refresh_token",
             "refresh_token": self.refresh_token}
 
-        auth = self.__authenticate(self.oauth)
-        self.__register_auth(auth)
-        self.__set_header(auth['access_token'])
+        auth = self._authenticate(self.oauth)
+        self._register_auth(auth)
+        self._set_header(auth['access_token'])
         logger.info("[+] Tokens refreshed")
-        self.__register_device_and_log_in()
+        self._register_device_and_log_in()
 
     def get_vehicles(self, headers):
         """Get vehicles for user"""
-        url = "%s/users/%s/vehicles?primaryOnly=true" % (IF9_BASE_URL, self.user_id)
-        return self.__open(url, headers)
+        url = f"{self.base.IF9}/users/{self.user_id}/vehicles?primaryOnly=true"
+        return self._request(url, headers)
 
     def get_user_info(self):
         """Get user information"""
-        return self.get(self.user_id, "%s/users" % IF9_BASE_URL, self.head)
+        return self.get(self.user_id, f"{self.base.IF9}/users", self.head)
 
     def update_user_info(self, user_info_data):
         """Update user information"""
         headers = self.head.copy()
         headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.User-v3+json; charset=utf-8"
-        return self.post(self.user_id, "%s/users" % IF9_BASE_URL, headers, user_info_data)
+        return self.post(self.user_id, f"{self.base.IF9}/users", headers, user_info_data)
 
     def reverse_geocode(self, lat, lon):
         """Get geocode information"""
-        return self.get("en",
-                        "%s/geocode/reverse/{0:f}/{1:f}".format(lat, lon) % IF9_BASE_URL,
-                        self.head)
+        return self.get("en", f"{self.base.IF9}/geocode/reverse/{lat}/{lon}", self.head)
 
 
 class Vehicle(dict):
@@ -209,25 +228,25 @@ class Vehicle(dict):
     def get_contact_info(self, mcc):
         """ Get contact info for the specified mobile country code"""
         headers = self.connection.head.copy()
-        return self.get('contactinfo/%s' % mcc, headers)
+        return self.get(f"contactinfo/{mcc}", headers)
 
     def get_attributes(self):
         """Get vehicle attributes"""
         headers = self.connection.head.copy()
-        headers["Accept"] = "application/vnd.ngtp.org.VehicleAttributes-v3+json"
+        headers["Accept"] = "application/vnd.ngtp.org.VehicleAttributes-v8+json"
         return self.get('attributes', headers)
 
     def get_status(self, key=None):
         """Get vehicle status"""
         headers = self.connection.head.copy()
-        headers["Accept"] = "application/vnd.ngtp.org.if9.healthstatus-v3+json"
+        headers["Accept"] = "application/vnd.ngtp.org.if9.healthstatus-v4+json"
         result = self.get('status?includeInactive=true', headers)
 
         if key:
-            coreStatusList = result['vehicleStatus']['coreStatus']
-            evStatusList = result['vehicleStatus']['evStatus']
-            coreStatusList = coreStatusList + evStatusList
-            return {d['key']: d['value'] for d in coreStatusList}[key]
+            core_status = result['vehicleStatus']['coreStatus']
+            ev_status = result['vehicleStatus']['evStatus']
+            core_status = core_status + ev_status
+            return {d['key']: d['value'] for d in core_status}[key]
 
         return result
 
@@ -235,7 +254,7 @@ class Vehicle(dict):
         """Get vehicle health status"""
         headers = self.connection.head.copy()
         headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.ServiceStatus-v4+json"
-        headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.StartServiceConfiguration-v3+json; charset=utf-8"
+        headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.StartServiceConfiguration-v3+json; charset=utf-8"  # noqa: E501, pylint: disable=line-too-long
 
         vhs_data = self._authenticate_vhs()
 
@@ -262,7 +281,7 @@ class Vehicle(dict):
         """Get the last 1000 trips associated with vehicle"""
         headers = self.connection.head.copy()
         headers["Accept"] = "application/vnd.ngtp.org.triplist-v2+json"
-        return self.get('trips?count=%d' % count, headers)
+        return self.get(f"trips?count={count}", headers)
 
     def get_guardian_mode_alarms(self):
         """Get Guardian Mode Alarms"""
@@ -298,7 +317,7 @@ class Vehicle(dict):
 
     def get_trip(self, trip_id, section=1):
         """Get info on a specific trip"""
-        return self.get('trips/%s/route?pageSize=1000&page=%d' % (trip_id, section), self.connection.head)
+        return self.get(f"trips/{trip_id}/route?pageSize=1000&page={section}", self.connection.head)
 
     def get_position(self):
         """Get current vehicle position"""
@@ -308,7 +327,7 @@ class Vehicle(dict):
         """Get service status"""
         headers = self.connection.head.copy()
         headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.ServiceStatus-v4+json"
-        return self.get('services/%s' % service_id, headers)
+        return self.get(f"services/{service_id}", headers)
 
     def get_services(self):
         """Get active services"""
@@ -318,7 +337,10 @@ class Vehicle(dict):
     def get_rcc_target_value(self):
         """Get Remote Climate Target Value"""
         headers = self.connection.head.copy()
-        return self.get('settings/ClimateControlRccTargetTemp', headers)
+        try:
+            return self.get('settings/ClimateControlRccTargetTemp', headers)
+        except HTTPError:
+            return None
 
     def set_attributes(self, nickname, registration_number):
         """Set vehicle nickname and registration number"""
@@ -345,7 +367,7 @@ class Vehicle(dict):
     def reset_alarm(self, pin):
         """Reset vehicle alarm"""
         headers = self.connection.head.copy()
-        headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.StartServiceConfiguration-v3+json; charset=utf-8"
+        headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.StartServiceConfiguration-v3+json; charset=utf-8"  # noqa: E501, pylint: disable=line-too-long
         headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.ServiceStatus-v4+json"
         aloff_data = self.authenticate_aloff(pin)
 
@@ -355,7 +377,7 @@ class Vehicle(dict):
         """Sound the horn and blink lights"""
         headers = self.connection.head.copy()
         headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.ServiceStatus-v4+json"
-        headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.StartServiceConfiguration-v3+json; charset=utf-8"
+        headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.StartServiceConfiguration-v3+json; charset=utf-8"  # noqa: E501, pylint: disable=line-too-long
 
         hblf_data = self.authenticate_hblf()
         return self.post("honkBlink", headers, hblf_data)
@@ -381,18 +403,19 @@ class Vehicle(dict):
         """Set Remote Climate Target Value (value between 31-57, 31 is LO 57 is HOT)"""
         headers = self.connection.head.copy()
         self.enable_provisioning_mode(pin)
-        service_parameters = {"key": "ClimateControlRccTargetTemp",
-                               "value": "%s" % str(target_value),
-                               "applied": 1}
+        service_parameters = {
+            "key": "ClimateControlRccTargetTemp",
+            "value": str(target_value),
+            "applied": 1
+        }
         self.post("settings", headers, service_parameters)
 
     def preconditioning_start(self, target_temp):
         """Start pre-conditioning for specified temperature (celsius)"""
-        service_parameters = [{"key": "PRECONDITIONING",
-                               "value": "START"},
-                              {"key": "TARGET_TEMPERATURE_CELSIUS",
-                               "value": "%s" % target_temp}]
-
+        service_parameters = [
+            {"key": "PRECONDITIONING", "value": "START"},
+            {"key": "TARGET_TEMPERATURE_CELSIUS", "value": str(target_temp)}
+        ]
         return self._preconditioning_control(service_parameters)
 
     def preconditioning_stop(self):
@@ -403,8 +426,7 @@ class Vehicle(dict):
 
     def climate_prioritize(self, priority):
         """Optimize climate controls for comfort or range"""
-        service_parameters = [{"key": "PRIORITY_SETTING",
-                               "value": "%s" % priority}]
+        service_parameters = [{"key": "PRIORITY_SETTING", "value": priority}]
         return self._preconditioning_control(service_parameters)
 
     def _preconditioning_control(self, service_parameters):
@@ -521,7 +543,7 @@ class Vehicle(dict):
         """Set the wakeup time for the specified time (epoch milliseconds)"""
         headers = self.connection.head.copy()
         headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.ServiceStatus-v3+json"
-        headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.StartServiceConfiguration-v3+json; charset=utf-8"
+        headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.StartServiceConfiguration-v3+json; charset=utf-8"  # noqa: E501, pylint: disable=line-too-long
         return self.post("swu", headers, swu_data)
 
     def enable_provisioning_mode(self, pin):
@@ -622,23 +644,23 @@ class Vehicle(dict):
         return self._authenticate_service(pin, "PROV")
 
     def authenticate_gm(self, pin):
+        """Authenticate to GM service"""
         return self._authenticate_service(pin, "GM")
 
     def _authenticate_service(self, pin, service_name):
         """Authenticate to specified service with the provided PIN"""
         data = {
-            "serviceName": "%s" % service_name,
-            "pin": "%s" % pin}
+            "serviceName": service_name,
+            "pin": str(pin)
+        }
         headers = self.connection.head.copy()
         headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.AuthenticateRequest-v2+json; charset=utf-8"
-
-        return self.post("users/%s/authenticate" % self.connection.user_id, headers, data)
-
-    def post(self, command, headers, data):
-        """Utility command to post data to VHS"""
-        return self.connection.post(command, '%s/vehicles/%s' % (IF9_BASE_URL, self.vin),
-                                    headers, data)
+        return self.post(f"users/{self.connection.user_id}/authenticate", headers, data)
 
     def get(self, command, headers):
         """Utility command to get vehicle data from API"""
-        return self.connection.get(command, '%s/vehicles/%s' % (IF9_BASE_URL, self.vin), headers)
+        return self.connection.get(command, f"{self.connection.base.IF9}/vehicles/{self.vin}", headers)
+
+    def post(self, command, headers, data):
+        """Utility command to post data to VHS"""
+        return self.connection.post(command, f"{self.connection.base.IF9}/vehicles/{self.vin}", headers, data)
