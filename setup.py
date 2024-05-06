ddb49d7fd23d7e870274b2769c16eb897af936ce
ardevd <edvard.holst@gmail.com>
2024-05-06 21:39:40 +0200

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding='UTF-8') as fh:
 
 setuptools.setup(
     name="jlrpy",
-    version="1.7.0",
+    version="1.7.1",
     author="ardevd",
     author_email="5gk633atf@relay.firefox.com",
     description="Control your Jaguar I-Pace",
